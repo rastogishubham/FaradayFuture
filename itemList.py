98655ee3ba752cd7f4d193494f1646fb40a561a5
Shubham S Rastogi <rastogis@ecegrid-thin4.ecn.purdue.edu>
2017-03-25 17:47:47 -0400

    Added testcases to code

--- itemList.py
+++ itemList.py
@@ -3,18 +3,21 @@
 
 class ItemList:
 
+    # Initialization of class
     def __init__(self, items_to_store):
-        self.item_list = []
         self.item_list = items_to_store
 
     def __len__(self):
         return len(self.item_list)
 
+    # Function to get the unique items of the list
     def get_unique_items(self):
         return list(set(self.item_list))
 
+    # Function to get the items and their frequencies from the list
     def get_item_and_frequency(self):
-        frequency_dictionary = {}
+        frequency_dictionary = {}  # Dictionary with key: item in list and value: Frequency of that item
+
         for item in self.item_list:
             if item not in frequency_dictionary:
                 frequency_dictionary[item] = 1
@@ -23,14 +26,49 @@ class ItemList:
 
         return frequency_dictionary
 
+    # Function to append a list of items or an item to the list
     def append_to_list(self, items):
         if type(items) is list:
             self.item_list.extend(items)
         else:
             self.item_list.append(items)
 
+    # Function to insert an item or a list of items in an index in the list
     def insert_in_list(self, items, index):
         if type(items) is list:
             self.item_list[index:index] = items
         else:
             self.item_list.insert(index, items)
+
+if __name__ == "__main__":
+    newList = ItemList([1, 2, 3])
+    print "List after initial initialization:"
+    print newList.item_list
+    newList.append_to_list(3)
+    print ''
+    print ''
+    print "List after appending 3 to it:"
+    print newList.item_list
+    newList.append_to_list([4, 4, 5, 6])
+    print ''
+    print ''
+    print "List after appending a list [4, 4, 5, 6] to it:"
+    print newList.item_list
+    newList.insert_in_list(6, 3)
+    print ''
+    print ''
+    print "List after inserting the number 6 at index 3:"
+    print newList.item_list
+    newList.insert_in_list([1, 1, 3], 4)
+    print ''
+    print ''
+    print "List after inserting the list [1, 1, 3] at index 4:"
+    print newList.item_list
+    print ''
+    print ''
+    print "Dictionary of items and their frequency:"
+    print newList.get_item_and_frequency()
+    print ''
+    print ''
+    print "List of all unique items:"
+    print newList.get_unique_items()
--- q2toq5.py
+++ q2toq5.py
@@ -3,10 +3,12 @@
 import re
 
 
+# Function to convert string to either int, float or string depending on input
 def convert(a=''):
-    int_pattern = r"^[0-9]+$"
-    float_pattern = r"^[0-9]*.[0-9]+$"
+    int_pattern = r"^[0-9]+$"  # Regex to check if input is an integer
+    float_pattern = r"^[0-9]*.[0-9]+$"  # Regex to check if input is a floating point number
 
+    # If it is an integer, or a floating point number convert to that type and return, otherwise return string
     if re.match(int_pattern, a):
         return int(a)
     elif re.match(float_pattern, a):
@@ -15,39 +17,67 @@ def convert(a=''):
         return a
 
 
+# Function to find minimum of three numbers without using inbuilt min function
 def find_minimum(num1, num2, num3):
-    minimum = num1
+    minimum = num1  # Set minimum to be first number
+
+    # if number 2 is less than minimum, set minimum to be number 2
     if num2 < minimum:
         minimum = num2
+    # if number 3 is less than minimum, set minimum to be number 3
     if num3 < minimum:
         minimum = num3
 
     return minimum
 
 
+# Function to apply operation on operands passed to the function
 def apply_operation(left_operand, right_operand, operator):
+    """ Creation of dictionary with lambda functions based on
+        operator input as key and the operation being
+        performed on the operands as the value
+    """
     operator_dict = {'+': (lambda x, y: x + y), '-': (lambda x, y: x - y), '*': (lambda x, y: x * y),
                      '/': (lambda x, y: float(x) / float(y))}
     return operator_dict[operator](left_operand, right_operand)
 
 
+# Function to reformat output based on input strings and print the output
 def reformat_output(animal, name, age):
     output = ('{name} the {animal} is {age} years old'.format(animal=animal, name=name, age=age))
     print output
 
+
 if __name__ == "__main__":
+    print "Testing Question 2:"
+    print ''
+    print ''
+    print "Test case 1: type(convert('1'))"
     print type(convert('1'))
+    print "Test case 2: type(convert('1.0'))"
     print type(convert('1.0'))
+    print "Test case 3: type(convert('asdf'))"
     print type(convert('asdf'))
-    print ""
-    print ""
-    reformat_output('dog', 'Fido', 10)
-    print ""
-    print ""
+    print ''
+    print ''
+    print "Testing Question 3:"
+    print "Test case: abc = {'dog', 'Fido', 10}"
+    abc = ['dog', 'Fido', 10]
+    reformat_output(abc[0], abc[1], abc[2])
+    print ''
+    print ''
+    print "Testing Question 4:"
+    print "Test case: find minimum of 4, 3, 5"
+    print "Minimum is:"
     print find_minimum(4, 3, 5)
     print ""
     print ""
+    print "Testing Question 5:"
+    print "Test case 1: apply_operation(2, 3, '+')"
     print apply_operation(2, 3, '+')
+    print "Test case 2: apply_operation(2, 3, '-')"
     print apply_operation(2, 3, '-')
+    print "Test case 3: apply_operation(2, 3, '*')"
     print apply_operation(2, 3, '*')
+    print "Test case 4: apply_operation(2, 3, '/')"
     print apply_operation(2.0, 3.0, '/')
